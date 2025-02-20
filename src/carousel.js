42f7798029bdd77611b6d30569622d9a01263c85
Vcjap <30173302+Vcjap@users.noreply.github.com>
2025-02-20 14:56:37 +0100

    Add function to go to next slide

--- src/carousel.js
+++ src/carousel.js
@@ -8,4 +8,16 @@ const addImagesToContainer = (imagesToAdd, container) => {
     });
 }
 
-export default {addImagesToContainer};
+const advanceSlide = (slideFrame, max= null) => {
+    const body = document.querySelector("body");
+    const computedStyle = getComputedStyle(body);
+    const pixelToAdvance = parseInt(computedStyle.getPropertyValue("--carousel-height"));
+    // need to use parseInt because the variable is returned as a string
+
+    const computedStyleSlideFrame = getComputedStyle(slideFrame);
+    const currentBottom = parseInt(computedStyleSlideFrame.bottom);
+
+    slideFrame.style.bottom = (currentBottom + pixelToAdvance) + "px"
+}
+
+export default {addImagesToContainer, advanceSlide};
--- src/index.js
+++ src/index.js
@@ -6,9 +6,6 @@ import winterImage2 from "./images/winter2.jpg";
 import winterImage3 from "./images/winter3.jpg";
 import winterImage4 from "./images/winter4.jpg";
 
-
-console.log("Hello World!")
-
 // Get the button and dropdown content
 const dropdownBtn = document.querySelector('.dropdown-button');
 const dropdownContent = document.querySelector('.dropdown-content');
@@ -28,7 +25,14 @@ window.addEventListener('click', (event) => {
 });
 
 
+//-------------Image carousel------------------
+
 const images = [winterImage1, winterImage2, winterImage3, winterImage4];
 
-const container = document.querySelector("#slides");
-carousel.addImagesToContainer(images, container);
+const slideFrame = document.querySelector("#slides");
+carousel.addImagesToContainer(images, slideFrame);
+
+const nextBtn = document.querySelector(".next");
+nextBtn.addEventListener("click", () => carousel.advanceSlide(slideFrame))
+
+// carousel.advanceSlide(slideFrame)
